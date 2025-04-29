be46902830117a894d78893fd124da72aba49381
KompelliSravanSyncfusion <kompellisravankumark@syncfusion.com>
2025-04-29 13:15:55 +0530

    Updated the sample with latest version Net 9

--- PieChartDemo/MainPage.xaml.cs
+++ PieChartDemo/MainPage.xaml.cs
@@ -1,14 +1,13 @@
-﻿using Syncfusion.Maui.Charts;
-using System.Globalization;
-
-namespace PieChartDemo
+﻿namespace PieChartDemo
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
         }
+      
     }
-}
 
+}
